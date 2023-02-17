f14ac2e37629f9124bfa6370bd85335094f8e87c
1234lentason <lentasonkevin@gmail.com>
2023-02-17 07:38:08 -0800

    Positive and Negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	int a;
+                
+	scanf("Enter n");
+	printf("The value of n is");
+
+         if n>0 {
+		 printf("n is positive");
+	 }
+	 else if (n=0){
+	 printf("is zero");
+	 }
+	 else (n<0){
+		 printf("is negative\n");
+	 }
+return (0);
+}
